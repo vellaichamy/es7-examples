9b252b0181968a95abde019db687b9245bb824c1
vellaichamy <uvellaichamy@gmail.com>
2021-11-07 22:50:03 +0530

    1 file modified

--- index.js
+++ index.js
@@ -63,5 +63,160 @@ var totalVegetables = Object.values(fruits).reduce((a, b) => a + b);
 
 console.log(totalVegetables);
 
+// =============================
+var list = [
+
+  "one",
+
+  "two",
+
+  "three",   // It is valid
+
+]
+
+var obj = {
+
+  one: "1",
+
+  two: "2",
+
+  three: "3", //  It is valid
+
+}
+
+function add(
+
+  one,
+
+  two,
+
+  three, // It is valid
+
+) {
+
+}
+
+
+for (var [key, val] of Object.entries(fruits)) {
+
+  console.log(key, val);
+
+}
+
+// Array.prototype.includes()
+console.clear();
+const array1 = [1, 2, 3];
+
+console.log(array1.includes(2));
+// expected output: true
+
+const pets = ['cat', 'dog', 'bat'];
+
+console.log(pets.includes('cat'));
+// expected output: true
+
+console.log(pets.includes('at'));
+// expected output: false
+
+const arr = [1, 2, 3, -1, NaN];
+const arr01  = ['1', '2', '3'];
+
+console.log(arr.includes(2))       // true
+console.log(arr.includes(4) )        // false
+console.log(arr.includes(3, 3)  )    // false
+console.log(arr.includes(3, -1) )    // true
+console.log(arr.includes(NaN)   )  // true
+console.log(arr01.includes(3) )  // false
+
+
+// ES9 Features ====== Rest/Spread properties =================
+console.clear();
+
+function sum(x, y, z) {
+  return x + y + z;
+}
+
+const numbers = [1, 2, 3];
+
+console.log(sum(...numbers));
+// expected output: 6
+
+console.log(sum.apply(null, numbers));
+// expected output: 6
+
+function sum(x, y, z, n) {
+  return x + y + z + n;
+}
+
+let numberStore = [0, 1, 2];
+let newNumber = 12;
+numberStore = [...numberStore, newNumber];
+
+
+console.log(numbers);
+
+
+function myFunction(x, y, z) { }
+let args = [0, 1, 2];
+myFunction.apply(null, args);
+console.log(myFunction());
+
+// 
+
+let arr1 = [0, 1, 2];
+let arr2 = [3, 4, 5];
+
+arr1 = [...arr1, ...arr2];
+//  arr1 is now [0, 1, 2, 3, 4, 5]
+console.log(arr1);
+
+let obj1 = { foo: 'bar', x: 42 };
+let obj2 = { foo: 'baz', y: 13 };
+const merge = ( ...objects ) => ( { ...objects } );
+
+let mergedObj1 = merge (obj1, obj2);
+// Object { 0: { foo: 'bar', x: 42 }, 1: { foo: 'baz', y: 13 } }
+
+let mergedObj2 = merge ({}, obj1, obj2);
+// Object { 0: {}, 1: { foo: 'bar', x: 42 }, 2: { foo: 'baz', y: 13 } }
+
+// ES10 Features =================================
+
+var arr02 = [1, 2, 3, 4];
+
+arr02.flatMap(x => [x, x * 2]);
+// is equivalent to
+arr02.reduce((acc, x) => acc.concat([x, x * 2]), []);
+// [1, 2, 2, 4, 3, 6, 4, 8]
+console.log('arr02',arr02);
+
+console.clear()
+
+let arr3 = [1, 2, 3, 4];
+
+
+
+console.log('arr3',...arr3.map(x => [x * 2]));
+// [[2], [4], [6], [8]]
+
+// [2, 4, 6, 8]
+console.log('arr3',arr3.flatMap(x => [x * 2]));
+// only one level is flattened
+
+// [[2], [4], [6], [8]]
+console.log('arr3',...arr3.flatMap(x => [[x * 2]]));
+
+
+
+let arr4 = ["it's Sunny in", "", "Bangalore"];
+
+console.log('arr4',arr4.map(x => x.split(" ")));
+// [["it's","Sunny","in"],[""],["Bangalore"]]
+
+console.log('arr4',arr4.flatMap(x => x.split(" ")));
+// ["it's","Sunny","in", "", "Bangalore"]
+
+
+
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>ES7 / ECMAScript 2016  Examples</h1>`;
