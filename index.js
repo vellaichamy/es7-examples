cfbb910f100e0f96ef6e16429337185f351f4544
vellaichamy <uvellaichamy@gmail.com>
2021-11-07 21:26:45 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,6 +1,51 @@
 // Import stylesheets
 import './style.css';
 
-// Write Javascript code!
+// [1, 2, 3].includes(-1)                   // false
+// [1, 2, 3].includes(1)                    // true
+// [1, 2, 3].includes(3, 4)                 // false
+// [1, 2, 3].includes(3, 3)                 // false
+// [1, 2, NaN].includes(NaN)                // true
+// ['foo', 'bar', 'quux'].includes('foo')   // true
+// ['foo', 'bar', 'quux'].includes('norf')  // false
+// ==============================================
+
+// es9  Async iterators
+function asyncIterator() {
+  const array = [1, 2];
+  return {
+    next: function() {
+      if (array.length) {
+        return Promise.resolve({
+          value: array.shift(),
+          done: false
+        });
+      } else {
+        return Promise.resolve({
+          done: true
+        });
+      }
+    }
+  };
+}
+
+var iterator = asyncIterator();
+
+(async function() {
+    await iterator.next().then(console.log); // { value: 1, done: false }
+    await iterator.next().then(console.log); // { value: 2, done: false }
+    await iterator.next().then(console.log); // { done: true }
+})();
+
+
+// Object rest properties =================================
+
+console.clear();
+
+let { fname, lname, ...rest } = { fname: "Ugra", lname: "UV", location: "Bangalore" };
+fname; //"Hemanth"
+lname; //"HM"
+rest; // {location: "Earth", type: "Human"}
+
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>ES7 / ECMAScript 2016  Examples</h1>`;
